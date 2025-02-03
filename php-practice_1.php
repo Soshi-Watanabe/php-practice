457fc95960400eb719d8d6c6142bf899c71790f7
Soshi-Watanabe <exe10pest@gmail.com>
2025-02-04 00:00:50 +0900

    Q6まで回答

--- php-practice_1.php
+++ php-practice_1.php
@@ -22,13 +22,32 @@ echo '現在時刻は' . date("Y年m月d日 H時i分s秒",$currentTimestamp) . '
 ?>
 
 // Q4 条件分岐-1 if文
+<?php
+$device ='mac';
+if ($device == 'windows' || $device == 'mac') {
+    if($device == 'windows'){
+        echo '使用OSは' . $device . 'です。';
+    } else {
+        echo '使用OSは' . $device . 'です。';
+    }
+} else {
+  echo 'どちらでもありません。';
+}
+?>
 
 
 // Q5 条件分岐-2 三項演算子
-
+<?php
+$age = 20;
+$message = ($age > 18) ? '成人です。' : '未成年です。';
+echo $message;
+?>
 
 // Q6 配列
-
+<?php
+$kanto = ['東京','埼玉','神奈川','千葉','群馬','栃木','茨城'];
+echo  ($kanto[2]) . 'と' . ($kanto[3]) . 'は関東地方の都道府県です。';
+?>
 
 // Q7 連想配列-1
 
