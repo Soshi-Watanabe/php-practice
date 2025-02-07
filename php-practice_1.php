3da296a684142e01aa7530e9cb38a681f644ed98
Soshi-Watanabe <exe10pest@gmail.com>
2025-02-07 22:05:25 +0900

    練習問題１仮終了

--- php-practice_1.php
+++ php-practice_1.php
@@ -50,24 +50,91 @@ echo  ($kanto[2]) . 'と' . ($kanto[3]) . 'は関東地方の都道府県です�
 ?>
 
 // Q7 連想配列-1
-
+<?php
+$mati = ['東京都' => '新宿区','神奈川県' => '横浜市','千葉県' => '千葉市','埼玉県' => 'さいたま市','栃木県' => '宇都宮市','群馬県' => '前橋市','茨城県' => '水戸市'];
+foreach ((array) $mati as $kenntyou ) {
+    echo $kenntyou."\r\n";
+}
+?>
 
 // Q8 連想配列-2
-
-
+<?php
+$mati = ['東京都' => '新宿区','神奈川県' => '横浜市','千葉県' => '千葉市','埼玉県' => 'さいたま市','栃木県' => '宇都宮市','群馬県' => '前橋市','茨城県' => '水戸市'];
+foreach ($mati as $kenntyou => $value) {
+    if ($kenntyou == '埼玉県') echo $kenntyou . 'の県庁所在地は' . $value . 'です。';
+}
+?>
 // Q9 連想配列-3
+<?php
+$mati = ['東京都' => '新宿区','神奈川県' => '横浜市','千葉県' => '千葉市','埼玉県' => 'さいたま市','栃木県' => '宇都宮市','群馬県' => '前橋市','茨城県' => '水戸市','北海道' => '札幌市', '沖縄県' => '那覇市'];
+foreach ((array) $mati as $kenntyou => $value) {
+  if ($kenntyou == '東京都' || $kenntyou == '神奈川県' || $kenntyou == '千葉県' || $kenntyou == '埼玉県' || $kenntyou == '栃木県' || $kenntyou == '群馬県' || $kenntyou == '茨城県' ) {
+    echo $kenntyou . 'の県庁所在地は' . $value . 'です。'."\r\n";
+  } else {
+      echo $kenntyou . 'は関東地方ではありません。'."\r\n";
+  }
+}
+?>
 
 
 // Q10 関数-1
-
-
+<?php
+function hello($name)
+{
+  echo $name . 'さん、こんにちは';  
+}
+hello('貴様');
+hello('わたし');
+?>
 // Q11 関数-2
-
+<?php
+function calcTaxInPrice($price)
+{
+  echo $price . '円の商品の税込み価格は' . $price*1.1 . '円です。';
+}
+calcTaxInPrice(1000);
+?>
 
 // Q12 関数とif文
-
+<?php
+function distinguishNum($number)
+{
+  if ($number % 2 == 0) {
+    echo $number . 'は偶数です';
+  } else {
+    echo $number . 'は奇数です';
+  }
+}
+distinguishNum(11);
+distinguishNum(44);
+?>
 
 // Q13 関数とswitch文
+<?php
+function evaluateGrade($rank)
+{
+  switch($rank) {
+    case 'A':
+      echo '合格です';
+      break;
 
+    case 'B' :
+      echo '合格です';
+      break;
 
+    case 'C' :
+      echo '合格ですが追加課題があります。';
+      break;
+
+    case 'D' :
+      echo '不合格です。';
+      break;
+
+    default :
+      echo '判定不明です。講師に問い合わせてください。';
+      break;
+  }
+}
+evaluateGrade('B')."\r\n";
+evaluateGrade('X')."\r\n";
 ?>
